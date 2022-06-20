641b9d6ff8e74406287e659f2f8d8b4a1bda144a
Rafael Menezes <menezes.rafa@gmail.com>
2022-06-20 16:31:08 -0300

    adicionando filtro por título de filme

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -1,15 +1,24 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace RazorPagesMovie.Models
 {
     public class Movie
     {
         public int ID { get; set; }
+
+        [Display(Name = "Título")]
         public string Title { get; set; } = string.Empty;
 
+        [Display(Name = "Data de lançamento")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
+
+        [Display(Name = "Gênero")]
         public string Genre { get; set; } = string.Empty;
+
+        [Display(Name = "Investimento")]
+        [Column(TypeName = "decimal(18,2)")]
         public decimal Price { get; set; }
     }
 }
--- Pages/Movies/Index.cshtml.cs
+++ Pages/Movies/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using RazorPagesMovie.Models;
 
@@ -13,18 +14,40 @@ namespace RazorPagesMovie.Pages_Movies
     {
         private readonly RazorPagesMovieContext _context;
 
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+        public SelectList Genres { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string MovieGenre { get; set; }
         public IndexModel(RazorPagesMovieContext context)
+
+        // construtor
         {
             _context = context;
         }
 
-        public IList<Movie> Movie { get;set; } = default!;
+        public IList<Movie> Movie { get; set; } = default!;
 
         public async Task OnGetAsync()
         {
+
             if (_context.Movie != null)
             {
-                Movie = await _context.Movie.ToListAsync();
+
+                var movies = from m in _context.Movie select m;
+
+                // se a string de busca (definida no bind property)
+                // estiver preenchida
+                if (!string.IsNullOrEmpty(SearchString))
+                {
+                    // cria uma LINQ que filtrará os filmes
+                    movies = movies.Where(s => s.Title.Contains(SearchString));
+                }
+
+                // a query só é executada neste ponto
+                Movie = await movies.ToListAsync();
+
             }
         }
     }
